f13dd6caaa4a49c2d90dd547dec4a78913b19e24
saxenabhishek <as7122000@gmail.com>
2021-05-05 04:53:23 +0530

    🚧 Trainer class

--- code/trainer.py
+++ code/trainer.py
@@ -2,3 +2,47 @@ r"""
 trainer
 """
 
+import code.models.disc as D
+import code.models.gen as G
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import tqdm
+
+
+class trainer:
+    def __init__(self, lr, dataloader) -> None:
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.vector = 512
+        self.gen = G.Generator(self.vector, 4, 1).to(self.device)
+        self.disc = D.Discriminator(1, 4).to(self.device)
+
+        self.gen.apply(self.weights_init)
+        self.disc.apply(self.weights_init)
+
+        beta1 = 0.5
+        self.optimD = optim.Adam(self.gen.parameters(), lr=lr, betas=(beta1, 0.999))
+        self.optimG = optim.Adam(self.gen.parameters(), lr=lr, betas=(beta1, 0.999))
+
+        self.dataloader = dataloader
+
+    def train(self, e):
+        for _ in range(e):
+            for i, data in enumerate(tqdm(self.dataloader), 0):
+                bz = data[0][0]
+                sample = data[0].to(self.device)
+
+                realD = self.disc(sample)
+
+                noise = noise = torch.randn(bz, self.vector, device=self.device)
+
+                self.disc.zero_grad()
+
+    def weights_init(m):
+        classname = m.__class__.__name__
+        if classname.find("Conv") != -1:
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+        elif classname.find("BatchNorm") != -1:
+            nn.init.normal_(m.weight.data, 1.0, 0.02)
+            nn.init.constant_(m.bias.data, 0)
--- test.py
+++ test.py
@@ -1,8 +1,31 @@
 import code.models.gen as G
+import code.models.disc as D
+import code.utils as utl
 import torch
+import torchvision
+from torchvision import transforms
+import matplotlib.pyplot as plt
+import random
+import numpy as np
 
 if __name__ == "__main__":
-    gen = G.Generator(512, 4)
-    print(gen)
-    o = gen(torch.rand(1, 512))
-    print(o.shape)
+    # gen = G.Generator(512, 4, 1)
+    # print(gen)
+    # o = gen(torch.rand(1, 512))
+    # print(o.shape)
+
+    # disc = D.Discriminator(1, 4)
+    # print(disc)
+    # o = disc(torch.rand(1, 1, 28, 28))
+    # print(o.mean())
+
+    t = transforms.Compose([transforms.ToTensor(), transforms.Normalize(0.5, 0.5)])
+
+    data = torchvision.datasets.MNIST("Data", train=True, download=True, transform=t)
+    dl = torch.utils.data.DataLoader(data, batch_size=25, shuffle=True)
+
+    print(len(dl))
+    r = random.randint(0, len(dl))
+    sample = next(iter(dl))
+    print(sample[0].shape)
+    utl.show_tensor_images(sample[0])
