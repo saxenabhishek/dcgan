869b33118d9c0c99253fa322e4821b8cec9d3a06
saxenabhishek <as7122000@gmail.com>
2021-05-05 01:56:09 +0530

    ✅ gen test

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-import code
+import code.models.gen as G
+import torch
 
-print(code)
+if __name__ == "__main__":
+    gen = G.Generator(512, 4)
+    print(gen)
+    o = gen(torch.rand(1, 512))
+    print(o.shape)
