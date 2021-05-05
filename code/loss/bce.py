05fe6eb3ded90a37f962edd903218603c42f125c
saxenabhishek <as7122000@gmail.com>
2021-05-05 18:35:33 +0530

    ✨ BCE loss

--- code/loss/bce.py
+++ code/loss/bce.py
@@ -2,3 +2,39 @@ r"""
 binnary cross entropy loss functions
 """
 
+from ctypes import set_last_error
+import torch
+from torch import nn
+from torch.autograd import backward
+
+
+class BCE:
+    criterion = nn.BCELoss()
+    truth_label = 1
+    fake_label = 0
+    loss_points = {
+        "d": [],
+        "g": [],
+    }
+
+    def disc(self, realD, fakeD) -> torch.Tensor:
+        reallab = torch.ones_like(realD) * self.truth_label
+
+        L_real = self.criterion(realD, reallab)
+        L_real.backward()
+
+        fakelab = reallab.fill_(self.fake_label)
+        L_fake = self.criterion(fakeD, fakelab)
+        L_fake.backward()
+
+        L = L_real + L_fake
+        self.loss_points["d"].append(L.mean().item())
+        return L_real + L_fake
+
+    def gen(self, GenfakeD):
+        reallab = torch.ones_like(GenfakeD) * self.truth_label
+        L_Fake = self.criterion(GenfakeD, reallab)
+        L_Fake.backward()
+        self.loss_points["g"].append(L_Fake.mean().item())
+        return L_Fake
+
