eba3faeeb7d68f615337a097e789cceb164af214
saxenabhishek <as7122000@gmail.com>
2021-05-06 00:24:25 +0530

    ⚗ Testing variations

--- dcgan/models/disc.py
+++ dcgan/models/disc.py
@@ -11,18 +11,22 @@ class Discriminator(nn.Module):
     ):
         super(Discriminator, self).__init__()
         self.main = nn.Sequential(self.block(c, filter * 2, first=True), self.block(filter * 2, filter * 4),)
-        self.last = nn.Sequential(nn.Conv2d(filter * 4, filter * 8, 4, 1, 0, bias=False), nn.Sigmoid())
+        self.last = nn.Sequential(
+            nn.Conv2d(filter * 4, filter * 8, 4, 2, 1, bias=False),
+            nn.Flatten(),
+            nn.Linear(filter * 8 * 7 * 7, 1),
+            nn.Sigmoid(),
+        )
 
     def block(self, i, o, first=False):
         l = []
-        l.append(nn.Conv2d(i, o, 4, 2 if not first else 1, 1 if not first else 0, bias=False))
+        l.append(nn.Conv2d(i, o, 4, 2 if not first else 1, 1 if not first else 2, bias=False))
         if not first:
             l.append(nn.BatchNorm2d(o))
         l.append(nn.LeakyReLU(0.2, inplace=True))
         return nn.Sequential(*l)
 
     def forward(self, input):
-        bs = input.shape[0]
         x = self.main(input)
         x = self.last(x)
         return x
--- dcgan/models/gen.py
+++ dcgan/models/gen.py
@@ -5,18 +5,14 @@ import torch.nn as nn
 
 
 class Generator(nn.Module):
-    def __init__(
-        self, z, filter, c
-    ):
+    def __init__(self, z, filter, c):
         super(Generator, self).__init__()
-        self.first = nn.Sequential(nn.Linear(z, 7 * 7 * z),)
-        self.main = nn.Sequential(
-            self.block(z, filter * 16), self.block(filter * 16, c, last=True)
-        )
+        self.first = nn.Sequential(nn.Linear(z, 7 * 7 * z), nn.ReLU(True))
+        self.main = nn.Sequential(self.block(z, filter * 16), self.block(filter * 16, c, last=True))
 
     def block(self, i, o, last=False):
         l = []
-        l.append(nn.ConvTranspose2d(i, o, 4, 2, 1, bias=False))
+        l.append(nn.ConvTranspose2d(i, o, 4, 2, 1, bias=True))
         if not last:
             l.append(nn.BatchNorm2d(o))
         l.append(nn.ReLU(True) if not last else nn.Tanh())
